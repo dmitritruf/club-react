eed0450412e50ac8501ee9988682086d70e1fadf
dmitritruf <gttim2@gmail.com>
2022-02-15 13:03:38 -0500

    brawler leaderboard

--- client/src/Pages/Brawlers.jsx
+++ client/src/Pages/Brawlers.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Paper from "@mui/material/Paper";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 
 import { TailSpin } from "react-loader-spinner";
 import { HashLink as Link } from "react-router-hash-link";
@@ -28,7 +30,12 @@ async function fetchBrawlers() {
 }
 
 function Brawlers({ members, club, mobileView }) {
+  const [display, setDisplay] = React.useState("brawlers");
   const [brawlers, setBrawlers] = React.useState([]);
+
+  const handleChange = (event, value) => {
+    setDisplay(value);
+  };
 
   React.useEffect(() => {
     async function boot() {
@@ -50,7 +57,23 @@ function Brawlers({ members, club, mobileView }) {
     return sorted[0];
   };
 
-  if (!brawlers.items || brawlers.items.length === 0) {
+  let leaders = [];
+  let leaderCounts = new Map();
+  let leaderboard = [];
+  if (brawlers.items && members.length !== 0) {
+    leaders = brawlers.items?.map((i) => ({
+      player: bestMember(i.name),
+      brawler: i.name,
+    }));
+    console.log(leaders);
+
+    leaders.forEach((i) => {
+      leaderCounts.set(i.player, (leaderCounts.get(i.player) ?? 0) + 1);
+    });
+    leaderboard = [...leaderCounts.entries()].sort((a, b) => b[1] - a[1]);
+  }
+
+  if (!brawlers.items || brawlers.items.length === 0 || members.length === 0) {
     return (
       <div className="container my-4">
         <Paper variant="outlined" style={{ background: "#121212AF" }}>
@@ -64,6 +87,71 @@ function Brawlers({ members, club, mobileView }) {
       </div>
     );
   }
+
+  const leaderTable = () => {
+    return (
+      <table
+        class="text-left"
+        style={{ fontSize: "20px", width: "100%", color: "#c0c0c0" }}
+      >
+        <thead>
+          <tr style={{ color: "white" }}>
+            {mobileView || (
+              <th class="text-center" style={{ width: "10%" }}>
+                Rank
+              </th>
+            )}
+            <th style={{ width: "25%" }}>Member</th>
+            <th>Brawlers</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboard?.map((i, count) => (
+            <tr key={count} class="hoverRow">
+              {mobileView || <td class="text-center">{count + 1}</td>}
+              <td>
+                <Link
+                  style={{ textDecoration: "none" }}
+                  to={`/members/${i[0].tag.substring(1)}`}
+                >
+                  <img
+                    src={`https://cdn.brawlify.com/profile-low/${i[0].icon.id}.png`}
+                    alt="badge"
+                    style={{ height: "1.3em" }}
+                  />
+                  <span
+                    class="mx-2"
+                    style={{
+                      color: `#${i[0].nameColor.substring(4)}`,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {i[0].name}
+                  </span>
+                </Link>
+              </td>
+              <td>
+                {leaders
+                  .filter((x) => x.player.name === i[0].name)
+                  .map((b) => (
+                    <img
+                      src={`https://cdn.brawlify.com/brawler/${capitalize(
+                        b.brawler
+                      )}.png`}
+                      alt="badge"
+                      style={{ height: "2em" }}
+                    />
+                  ))}{" "}
+                <span style={{ color: "gold", fontWeight: "bold" }}>
+                  {leaders.filter((x) => x.player.name === i[0].name).length}
+                </span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  };
 
   return (
     <div className="container my-4">
@@ -82,69 +170,79 @@ function Brawlers({ members, club, mobileView }) {
             >
               BRAWLER SPECIALISTS
             </span>
-            {/* {mobileView && <br />}
-            <Autocomplete
-              disablePortal
-              options={club.members?.map((i) => i.name)}
-              sx={{ width: 300 }}
-              renderInput={(params) => (
-                <TextField {...params} size="small" label="Search Member" />
-              )}
-            /> */}
+            <ToggleButtonGroup
+              size="small"
+              value={display}
+              exclusive
+              onChange={handleChange}
+              aria-label="text alignment"
+            >
+              <ToggleButton value="brawlers" aria-label="left aligned">
+                BRAWLERS
+              </ToggleButton>
+              <ToggleButton value="leaders" aria-label="centered">
+                LEADERS
+              </ToggleButton>
+            </ToggleButtonGroup>
           </div>
-          <div class="my-3 brawlerGallery">
-            {brawlers.items
-              ?.sort((a, b) => a.name.localeCompare(b.name))
-              .map((i) => (
-                <div>
+          {display === "brawlers" && (
+            <div class="my-3 brawlerGallery">
+              {brawlers.items
+                ?.sort((a, b) => a.name.localeCompare(b.name))
+                .map((i) => (
                   <div>
+                    <div>
+                      <img
+                        src="https://cdn.brawlify.com/icon/trophy.png"
+                        alt="trophy"
+                        style={{ height: "1.2em" }}
+                      />
+                      <span
+                        class="mx-1"
+                        style={{
+                          textOverflow: "ellipsis",
+                          overflow: "hidden",
+                          color: "gold",
+                          fontWeight: "bold",
+                          whiteSpace: "nowrap",
+                        }}
+                      >
+                        {
+                          bestMember(i.name)?.brawlers.find(
+                            (x) => x.name === i.name
+                          )?.trophies
+                        }
+                      </span>
+                    </div>
                     <img
-                      src="https://cdn.brawlify.com/icon/trophy.png"
-                      alt="trophy"
-                      style={{ height: "1.2em" }}
+                      src={`https://cdn.brawlify.com/brawler/${capitalize(
+                        i.name
+                      )}.png`}
+                      alt="badge"
+                      style={{ height: "5em" }}
                     />
-                    <span
-                      class="mx-1"
-                      style={{
-                        textOverflow: "ellipsis",
-                        overflow: "hidden",
-                        color: "gold",
-                        fontWeight: "bold",
-                        whiteSpace: "nowrap",
-                      }}
+                    <br />
+                    <Link
+                      to={`/members/${bestMember(i.name)?.tag.substring(1)}`}
+                      style={{ textDecoration: "none" }}
                     >
-                      {
-                        bestMember(i.name)?.brawlers.find(
-                          (x) => x.name === i.name
-                        )?.trophies
-                      }
-                    </span>
+                      <span
+                        style={{
+                          color: `#${bestMember(i.name)?.nameColor.substring(
+                            4
+                          )}`,
+                          fontWeight: "bold",
+                          whiteSpace: "nowrap",
+                        }}
+                      >
+                        {bestMember(i.name)?.name.substring(0, 10)}
+                      </span>
+                    </Link>
                   </div>
-                  <img
-                    src={`https://cdn.brawlify.com/brawler/${capitalize(
-                      i.name
-                    )}.png`}
-                    alt="badge"
-                    style={{ height: "5em" }}
-                  />
-                  <br />
-                  <Link
-                    to={`/members/${bestMember(i.name)?.tag.substring(1)}`}
-                    style={{ textDecoration: "none" }}
-                  >
-                    <span
-                      style={{
-                        color: `#${bestMember(i.name)?.nameColor.substring(4)}`,
-                        fontWeight: "bold",
-                        whiteSpace: "nowrap",
-                      }}
-                    >
-                      {bestMember(i.name)?.name.substring(0, 10)}
-                    </span>
-                  </Link>
-                </div>
-              ))}
-          </div>
+                ))}
+            </div>
+          )}
+          {display === "leaders" && <div class="my-3">{leaderTable()}</div>}
         </div>
       </Paper>
     </div>
